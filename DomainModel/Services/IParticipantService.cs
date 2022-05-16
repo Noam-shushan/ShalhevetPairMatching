07aa4504a374998b2f70eb973727240d49daf533
Noam shushan <noam8shu@gmail.com>
2022-05-16 12:39:34 +0300

    code cleanup

--- DomainModel/Services/IParticipantService.cs
+++ DomainModel/Services/IParticipantService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PairMatching.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace PairMatching.DomainModel.Services
 {
     public interface IParticipantService
     {
-
+        Task<IEnumerable<Participant>> GetAllParticipants();
     }
 }
