98b5ae97de9528a5bc3248e24766a96d83c6a1d4
Noam Shushan <noam8shu@gmail.com>
2023-05-15 12:00:10 +0300

    refactor and organize folders

---
+++ Models/CurrentModels/Participaints/OpenQuestionsForIsrael.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PairMatching.Models
+{
+    public class OpenQuestionsForIsrael
+    {
+        public string PersonalTraits { get; set; } = "";
+
+        public string BiographHeb { get; set; } = "";
+
+        public string WhoIntroduced { get; set; } = "";
+
+        public string WhyJoinShalhevet { get; set; } = "";
+
+        public string AdditionalInfo { get; set; } = "";
+
+        public string GeneralInfo { get; set; } = "";
+    }
+}
---
+++ Models/CurrentModels/Participaints/OpenQuestionsForWorld.cs
@@ -0,0 +1,26 @@
+namespace PairMatching.Models
+{
+    public class OpenQuestionsForWorld
+    {
+        public string WhoIntroduced { get; set; } = "";
+
+        public string AdditionalInfo { get; set; } = "";
+
+        public List<string> HopesExpectations { get; set; } = new ();
+
+        public string AnythingElse { get; set; } = "";
+
+        public string ConversionRabi { get; set; } = "";
+
+        public string RequestsFromPair { get; set; } = "";
+
+        public string Experience { get; set; } = "";
+
+        public string PersonalBackground { get; set; } = "";
+
+        public string JewishAndComAff { get; set; } = "";
+
+        public string GeneralInfo { get; set; } = "";
+
+    }
+}    
